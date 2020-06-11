6395613cd273edd3317b75c81935b5c120f0b454
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-06-11 23:15:02 +0200

    Zadanie 1a gotowe obliczające silnię metodą rekurencyjną

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using System.Numerics;
+using System;
 
 namespace Programowanie_zadania_na_zaliczenie
 {
     class Program
     {
+        public static BigInteger silnia(BigInteger n)
+        {
+            if (n < 2)
+                return 1;
+            else
+                return n * silnia(n - 1);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BigInteger i;
+            Console.WriteLine("Pdaj liczbę z której chcesz obliczyć silnię: ");
+            i = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik to: " + silnia(i));
         }
     }
 }
