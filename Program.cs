74d84008bb085d37a7d7637e834e660761c4768b
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-06-15 17:31:43 +0200

    zadanie 5 gotowe

--- Program.cs
+++ Program.cs
@@ -6,7 +6,85 @@ namespace Programowanie_zadania_na_zaliczenie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int wybór, w1;
+            double cena=0, cukier;
+            Console.WriteLine("Witaj! Wybierz napój, który chcesz kupić: ");
+            Console.WriteLine("1. Kawa 1,50 zł");
+            Console.WriteLine("2. Herbata 1,00 zł");
+            Console.WriteLine("3. Czekolada 2,00 zł");
+            wybór =int.Parse (Console.ReadLine());
+            
+            switch (wybór)
+            {
+                case 1:
+                    {
+                        cena = 1.50;
+                        Console.WriteLine("Wybrano kawę");
+                        Console.WriteLine("Chcesz kupić cukier za 0,5 zł?");
+                        Console.WriteLine("1. Tak");
+                        Console.WriteLine("2. Nie");
+                        w1 = int.Parse(Console.ReadLine());
+                        if (w1 == 1)
+                        {
+                            cukier=0.5;
+                            cena += cukier;
+                            Console.WriteLine("Cena to " + cena+" zł");
+                            break;
+                        }
+                        else
+                        {
+                            cukier = 0;
+                            Console.WriteLine("Cena to " + cena+" zł");
+                            break;
+                            
+                        }
+                    }
+                case 2:
+                    {
+                        cena = 1.00;
+                        Console.WriteLine("Wybrano herbatę");
+                        Console.WriteLine("Chcesz kupić cukier za 0,5 zł?");
+                        Console.WriteLine("1. Tak");
+                        Console.WriteLine("2. Nie");
+                        w1 = int.Parse(Console.ReadLine());
+                        if (w1 == 1)
+                        {
+                            cukier = 0.5;
+                            cena += cukier;
+                            Console.WriteLine("Cena to: " + cena + " zł");
+                            break;
+                        }
+                        else
+                        {
+                            cukier = 0;
+                            Console.WriteLine("Cena to: " + cena + " zł");
+                            break;
+                        }
+                    }
+                case 3:
+                    {
+                        cena = 2.00;
+                        Console.WriteLine("Wybrano czekoladę");
+                        Console.WriteLine("Chcesz kupić cukier za 0,5 zł?");
+                        Console.WriteLine("1. Tak");
+                        Console.WriteLine("2. Nie");
+                        w1 = int.Parse(Console.ReadLine());
+                        if (w1 == 1)
+                        {
+                            cukier=0.5;
+                            cena += cukier;
+                            Console.WriteLine("Cena to: " + cena + " zł");
+                            break;
+                        }
+                        else
+                        {
+                            cukier = 0;
+                            Console.WriteLine("Cena to: " + cena + " zł");
+                            break;
+                        }
+                    }
+            }
+            Console.ReadLine();
         }
     }
 }
